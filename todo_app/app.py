c0383248c113d9ce153fd65014c82e48a2165065
Aarav Arora <aaravarora.works@gmail.com>
2023-02-15 14:05:47 +0530

    feat: dockerizing the application

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
-
+import os
 from api_blueprint import api
-
+from gunicorn import glogging
 app = Flask(__name__)
+
+
+# custom error handler
+@app.errorhandler(404)
+def not_found(e):
+    return {"error": "404 Not Found"}
 
 # /api blueprint for todo api's
 app.register_blueprint(api)
+
+
+
+
+
